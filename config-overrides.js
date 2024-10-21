bad618639498bcbdf024e35339b110f15db5768b
Yannik Heinz <hyannik02@gmail.com>
2024-10-21 13:35:00 +0200

    Update

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,13 @@
-module.exports = function override(config, env) {
+const path = require('path');
+
+module.exports = function override(config) {
+  config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+  config.resolve.alias['@/components'] = path.resolve(__dirname, 'src/components');
+  
   config.resolve.fallback = {
     ...config.resolve.fallback,
     fs: require.resolve('./src/mocks/fs.js'),
   };
+  
   return config;
 };
